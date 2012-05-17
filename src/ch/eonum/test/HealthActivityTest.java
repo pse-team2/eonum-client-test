63c3e573327bcf7d459c016a4bbb0faa445df813
Renato Corti <renato.corti@students.unibe.ch>
2012-05-17 19:57:17 +0200

    Provide new tests
    	modified:   res/layout/main.xml
    	new file:   src/ch/eonum/test/HealthActivityTest.java
    	modified:   src/ch/eonum/test/HealthTest.java

--- src/ch/eonum/test/HealthActivityTest.java
+++ src/ch/eonum/test/HealthActivityTest.java
@@ -1,7 +1,9 @@
 package ch.eonum.test;
 
-import ch.eonum.StartActivity;
-import android.app.Activity;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+
+import ch.eonum.HealthActivity;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.location.Criteria;
@@ -10,51 +12,86 @@ import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
-import android.test.suitebuilder.annotation.Smoke;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapView;
-import com.jayway.android.robotium.solo.Solo;
-
-public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
+public class HealthActivityTest extends ActivityInstrumentationTestCase2<HealthActivity>
 {
+	private HealthActivity mActivity; // The activity under test
+	private TextView whatView, whereView; // The activity's AutoCompleteTextView
+	private String whatString, whereString;
+	private AutoCompleteTextView whatText, whereText;
+	private MenuItem mAbout;
+	private MapView mapView;
+	private Button searchButton;
+	private ImageButton positionButton;
 
-	private Solo solo;
-
-	public HealthTest()
+	/**
+	 * This constructor has no parameters, and its sole purpose is to pass information to the superclass's
+	 * default constructor.
+	 */
+	public HealthActivityTest()
 	{
-		super("ch.eonum", StartActivity.class);
+		super("ch.eonum", HealthActivity.class);
 	}
+	// End of HealthActivityTest constructor definition
 
 	@Override
-	public void setUp()
+	protected void setUp() throws Exception
 	{
-		solo = new Solo(getInstrumentation(), getActivity());
+		/* Invokes the superclass constructor for setUp(), which is required by JUnit. */
+		super.setUp();
+
+		/* This turns off touch mode in the device or emulator. If any of your test methods send key events to
+		 * the application, you must turn off touch mode before you start any activities; otherwise, the call
+		 * is ignored. */
+		setActivityInitialTouchMode(false);
+
+		// Gets a reference to the activity under test (HealthActivity).
+		// This call also starts the activity if it is not already running.
+		this.mActivity = this.getActivity();
+
+		// Gets a reference to the MapView
+		this.mapView = (MapView) this.mActivity.findViewById(ch.eonum.R.id.mapview);
+
+		this.whatView = (TextView) this.mActivity.findViewById(ch.eonum.R.id.textviewWhat);
+		this.whatText = (AutoCompleteTextView) this.mActivity.findViewById(ch.eonum.R.id.searchforWhat);
+		this.whereView = (TextView) this.mActivity.findViewById(ch.eonum.R.id.textviewWhere);
+		this.whereText = (AutoCompleteTextView) this.mActivity.findViewById(ch.eonum.R.id.searchforWhere);
+		this.whatString = this.mActivity.getString(ch.eonum.R.string.what);
+		this.whereString = this.mActivity.getString(ch.eonum.R.string.where);
+
+		this.positionButton = (ImageButton) this.mActivity.findViewById(ch.eonum.R.id.getposition);
+		this.searchButton = (Button) this.mActivity.findViewById(ch.eonum.R.id.search);
+
+		// Gets a reference to the menu of the application under test.
+		this.mAbout = (MenuItem) this.mActivity.findViewById(ch.eonum.R.id.menu_mAbout);
 	}
+	// End of setUp() method definition
 
-	@Override
-	public void tearDown()
+	/**
+	 * The initial conditions test verifies that the application under test is initialized correctly.
+	 * It is an illustration of the types of tests you can run, so it is not comprehensive.
+	 */
+	public void testPreconditions()
 	{
-		// Robotium will finish all the activities that have been opened
-		solo.finishOpenedActivities();
+		assertNotNull(this.mActivity);
+		assertNotNull(this.whatText);
+		assertNotNull(this.whereText);
+		assertNotNull(this.whatView);
+		assertNotNull(this.whereView);
+		assertNotNull(this.mAbout);
 	}
+	// End of testPreConditions() method definition
 
-	@Smoke
-	public void testAboutActivity()
+	public void testText()
 	{
-		// Assert HealthActivity
-		solo.assertCurrentActivity("Expected StartActivity activity", "StartActivity");
-		solo.clickOnMenuItem("About", true);
-
-		// Assert About Activity
-		solo.assertCurrentActivity("Expected About activity", "About");
-
-		// Assert that text is found
-		assertTrue("Text 'Arztsuche' not found", solo.searchText("Arztsuche"));
-		assertTrue("Text 'eonum' not found", solo.searchText("eonum"));
+		assertEquals(this.whatString, (String) this.whatView.getText());
+		assertEquals(this.whereString, (String) this.whereView.getText());
 	}
 
 	@UiThreadTest
@@ -64,11 +101,8 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 		 * This allows the method to change the state of the AutoCompleteTextView in the application under test.
 		 * This use of @UiThreadTest shows that, if necessary, you can run an entire method on the UI thread. */
 
-		Activity currentActivity = solo.getCurrentActivity();
-		assertNotNull("Current activity is null", currentActivity);
-		AutoCompleteTextView tv = (AutoCompleteTextView) solo.getView(ch.eonum.R.id.searchforWhat);
-		assertNotNull("TextView is null", tv);
-		solo.clickOnEditText(1);
+		assertTrue(this.whatText.performClick());
+
 		/* Send key events to the AutoCompleteTextView searchForWhat to select one of the items.
 		 * To do this, open the AutoCompleteTextView by "pressing" the "A" button
 		 * (sending a KeyCode event) and then clicking (sending) the down arrow keypad button four times.
@@ -84,8 +118,8 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 		/* Query the current state of the AutoCompleteTextView searchForWhat
 		 * and compare its current selection to the expected value.
 		 * Call the method getText() to find out the text that is being displayed in the text field. */
-		String selection = solo.getEditText(1).getText().toString();
-		String stringResource = solo.getString(ch.eonum.R.string.allgemeinaerzte);
+		String selection = this.whatText.getText().toString();
+		String stringResource = this.mActivity.getString(ch.eonum.R.string.allgemeinaerzte);
 
 		// Assert that this string value matches the expected value of "Allgemeinärzte".
 		assertEquals(stringResource, selection);
@@ -95,21 +129,21 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 	{
 		double lat = 46.9513449;
 		double lng = 7.4384537;
-		Activity currentActivity = solo.getCurrentActivity();
-		LocationManager testLocMgr = (LocationManager) currentActivity.getSystemService(Context.LOCATION_SERVICE);
+		LocationManager testLocMgr = (LocationManager) this.mActivity.getSystemService(Context.LOCATION_SERVICE);
 		testLocMgr.addTestProvider("TestProvider", false, false, false, false, false, false, false,
 			Criteria.POWER_LOW, Criteria.ACCURACY_FINE);
 		testLocMgr.setTestProviderEnabled("TestProvider", true);
 
 		// Set up test
+
 		Location location = new Location("TestProvider");
 		location.setLatitude(lat);
 		location.setLongitude(lng);
 		testLocMgr.setTestProviderLocation("TestProvider", location);
 
 		// Check if your listener reacted the right way
-		double currentLatitude = ((ch.eonum.HealthActivity) currentActivity).getLocation().getLatitude();
-		double currentLongitude = ((ch.eonum.HealthActivity) currentActivity).getLocation().getLongitude();
+		double currentLatitude = mActivity.getLocation().getLatitude();
+		double currentLongitude = mActivity.getLocation().getLongitude();
 
 		assertEquals(lat, currentLatitude);
 		assertEquals(lng, currentLongitude);
@@ -124,9 +158,7 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 		double lat2 = 46.94809;
 		double lng2 = 7.44744;
 
-		Activity currentActivity = solo.getCurrentActivity();
-		MapView map = (MapView) solo.getView(ch.eonum.R.id.mapview);
-		LocationManager testLocMgr = (LocationManager) currentActivity.getSystemService(Context.LOCATION_SERVICE);
+		LocationManager testLocMgr = (LocationManager) this.mActivity.getSystemService(Context.LOCATION_SERVICE);
 		testLocMgr.addTestProvider("TestProvider", false, false, false, false, false, false, false,
 			Criteria.POWER_LOW, Criteria.ACCURACY_FINE);
 		testLocMgr.setTestProviderEnabled("TestProvider", true);
@@ -137,37 +169,37 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 		location.setLongitude(lng1);
 		testLocMgr.setTestProviderLocation("TestProvider", location);
 
-		GeoPoint initialCenterPoint = map.getMapCenter();
+		GeoPoint initialCenterPoint = this.mapView.getMapCenter();
 
 		testLocMgr.removeTestProvider("TestProvider");
 
 		// Verify if the MapView has changed
-		assertEquals((int)(lat1 * 1000000), map.getMapCenter().getLatitudeE6());
-		assertEquals((int)(lng1 * 1000000), map.getMapCenter().getLongitudeE6());
+		assertEquals((int)(lat1 * 1000000), this.mapView.getMapCenter().getLatitudeE6());
+		assertEquals((int)(lng1 * 1000000), this.mapView.getMapCenter().getLongitudeE6());
 
 		// Go to a different location
 		GeoPoint differentPoint = new GeoPoint((int)(lat2 * 1000000), (int)(lng2 * 1000000));
-		map.getController().setCenter(differentPoint);
+		this.mapView.getController().setCenter(differentPoint);
 
 		assertNotSame(initialCenterPoint.getLatitudeE6(), differentPoint.getLatitudeE6());
 		assertNotSame(initialCenterPoint.getLongitudeE6(), differentPoint.getLongitudeE6());
 
 		// Terminate the activity and restart it
-		currentActivity.finish();
-		currentActivity = this.getActivity();
+		this.mActivity.finish();
+		this.mActivity = this.getActivity();
 
 		// Verify if the MapView has restored its state
-		assertEquals(differentPoint.getLatitudeE6(), map.getMapCenter().getLatitudeE6());
-		assertEquals(differentPoint.getLongitudeE6(), map.getMapCenter().getLongitudeE6());
+		assertEquals(differentPoint.getLatitudeE6(), this.mapView.getMapCenter().getLatitudeE6());
+		assertEquals(differentPoint.getLongitudeE6(), this.mapView.getMapCenter().getLongitudeE6());
 
 		// Reset the map view to initialCenterPoint clicking the my location button
-		solo.clickOnImageButton(0);
+		this.positionButton.performClick();
 
-		assertEquals(initialCenterPoint.getLatitudeE6(), map.getMapCenter().getLatitudeE6());
-		assertEquals(initialCenterPoint.getLongitudeE6(), map.getMapCenter().getLongitudeE6());
+		assertEquals(initialCenterPoint.getLatitudeE6(), this.mapView.getMapCenter().getLatitudeE6());
+		assertEquals(initialCenterPoint.getLongitudeE6(), this.mapView.getMapCenter().getLongitudeE6());
 
-		double currentLatitude = ((ch.eonum.HealthActivity) currentActivity).getLocation().getLatitude();
-		double currentLongitude = ((ch.eonum.HealthActivity) currentActivity).getLocation().getLongitude();
+		double currentLatitude = mActivity.getLocation().getLatitude();
+		double currentLongitude = mActivity.getLocation().getLongitude();
 
 		assertEquals(currentLatitude, initialCenterPoint.getLatitudeE6());
 		assertEquals(currentLongitude, initialCenterPoint.getLongitudeE6());
@@ -175,8 +207,7 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 
 	public void testLocationManagerState()
 	{
-		Activity currentActivity = solo.getCurrentActivity();
-		LocationManager testLocMgr = (LocationManager) currentActivity.getSystemService(Context.LOCATION_SERVICE);
+		LocationManager testLocMgr = (LocationManager) mActivity.getSystemService(Context.LOCATION_SERVICE);
 		testLocMgr.addTestProvider("TestProvider", false, false, false, false, false, false, false,
 			Criteria.POWER_LOW, Criteria.ACCURACY_FINE);
 		testLocMgr.setTestProviderEnabled("TestProvider", true);
@@ -189,7 +220,7 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 
 		// Use instrumentation to call the Activity's onPause()
 
-		mInstr.callActivityOnPause(currentActivity);
+		mInstr.callActivityOnPause(mActivity);
 
 		/* Under test, the activity is waiting for input. The invocation of
 		 * callActivityOnPause(android.app.Activity) performs a call directly to the activity's onPause()
@@ -202,7 +233,7 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 		 * leaving it as it was. */
 
 		// Use instrumentation to call the Activity's onResume():
-		mInstr.callActivityOnResume(currentActivity);
+		mInstr.callActivityOnResume(mActivity);
 
 		/* Invoking callActivityOnResume(android.app.Activity) affects the activity in a way similar to
 		 * callActivityOnPause. The activity's onResume() method is invoked instead of manipulating the
@@ -214,12 +245,9 @@ public class HealthTest extends ActivityInstrumentationTestCase2<StartActivity>
 
 	public void testSearch()
 	{
-		Button searchbutton = solo.getButton(0);
-		assertNotNull(searchbutton);
-		AutoCompleteTextView tv = (AutoCompleteTextView) solo.getView(ch.eonum.R.id.searchforWhere);
-		assertNotNull("TextView is null", tv);
-		solo.typeText(0, "Tscharnerstrasse 41 3007 Bern");
-		solo.clickOnButton(1);
+		this.whereText.setText("Tscharnerstrasse 41 3007 Bern");
+		this.searchButton.performClick();
 		//TODO: Test MapView
 	}
+
 }
